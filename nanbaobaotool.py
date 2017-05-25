65b62f2758cc759a3916ad23a9621c0a2757f6ce
unknowy <zhaihaoyuzhy@163.com>
2017-05-25 23:24:04 +0800

    完成安全月报第一部分

--- nanbaobaotool.py
+++ nanbaobaotool.py
@@ -8,74 +8,25 @@ from docx import Document
 from docx.shared import Pt
 from docx.shared import Inches
 from docx.oxml.ns import qn
-def doc():
-    document = Document()
-    #加入不同等级的标题
-    head=document.add_heading(u'信息安全月报添加部分',0)
 
-    # document.add_heading(u'一级标题',1)
-    # document.add_heading(u'二级标题',2)
-    #添加文本
-    paragraph = document.add_paragraph(u'           ')
-    # #设置字号
-    # run = paragraph.add_run(u'设置字号、')
-    # run.font.size = Pt(24)
-    # #设置字体
-    # run = paragraph.add_run('Set Font,')
-    # run.font.name = 'Consolas'
-    #设置中文字体
-    run = paragraph.add_run(u'本月共扫描内网终端2709台，发现10个高危、31个中危，已整改高危3个，中10个，其余已列入4月整改计划。本月共扫描外网桌面终端41台，无中、高危漏洞。具体扫描结果见附件。、')
-    run.font.name=u'方正仿宋_GBK'
-    run.font.size = Pt(20)
+
+def doc(word):
+    document = Document('report.docx')
+    paragraph = document.add_paragraph('    ')
+    run = paragraph.add_run(word)
+    run.font.name = u'宋体'
     r = run._element
-    r.rPr.rFonts.set(qn('w:eastAsia'), u'方正仿宋_GBK')
-    #设置斜体
-    run = paragraph.add_run(u'斜体、')
-    run.italic = True
-    #设置粗体
-    run = paragraph.add_run(u'粗体').bold = True
-    #增加无序列表
-    document.add_paragraph(
-    u'无序列表元素1', style='List Bullet'
-    )
-    document.add_paragraph(
-    u'无序列表元素2', style='List Bullet'
-    )
-    #增加有序列表
-    document.add_paragraph(
-    u'有序列表元素1', style='List Number'
-    )
-    document.add_paragraph(
-    u'有序列表元素2', style='List Number'
-    )
-    #增加图像（此处用到图像image.bmp，请自行添加脚本所在目录中）
-
-    #document.add_picture('1.png')
+    r.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
+    document.save(u'report.docx')
 
 
-    #增加表格
-    table = document.add_table(rows=1, cols=3)
-    hdr_cells = table.rows[0].cells
-    hdr_cells[0].text = 'Name'
-    hdr_cells[1].text = 'Id'
-    hdr_cells[2].text = 'Desc'
-    #再增加3行表格元素
-    for i in xrange(3):
-        row_cells = table.add_row().cells
-        row_cells[0].text = 'test'+str(i)
-        row_cells[1].text = str(i)
-        row_cells[2].text = 'desc'+str(i)
-    #增加分页
-    document.add_page_break()
-    #保存文件
-    document.save(u'信息安全月报.doc')
 def file_name(file_dir):
     L = []
     for root, dirs, files in os.walk(file_dir):
         for file in files:
             if file.endswith(".zip"):
 
-                file_name = os.path.join(root,file)
+                file_name = os.path.join(root, file)
                 L.append(file_name)
     return L
 
@@ -110,26 +61,142 @@ def write_excel(excel):
 
 def read_excel(excel_name):
     book = xlrd.open_workbook(excel_name)
-
-    sheet_name = book.sheet_names()[0]  # 获得指定索引的sheet名字
-    # print sheet_name
+    sheet_name = book.sheet_names()[1]  # 获得指定索引的sheet名字
     sheet = book.sheet_by_name(sheet_name)
-    cell_value1 = sheet.cell_value(2, 2)
-    print cell_value1
+    col_data = sheet.col_values(1)
+    risk_data = []
+    try:
+        row_portal_server = col_data.index('10.232.80.20')
+        portal_server_hrisk = int(sheet.cell_value(row_portal_server, 5))
+        portal_server_mrisk = int(sheet.cell_value(row_portal_server, 6))
+        risk_data.append(portal_server_hrisk)
+        risk_data.append(portal_server_mrisk)
+    except ValueError:
+        portal_server_hrisk = 0
+        portal_server_mrisk = 0
+        risk_data.append(portal_server_hrisk)
+        risk_data.append(portal_server_mrisk)
+    try:
+        row_backup_server = col_data.index('10.232.82.65')
+        backup_server_hrisk = int(sheet.cell_value(row_backup_server, 5))
+        backup_server_mrisk = int(sheet.cell_value(row_backup_server, 6))
+        risk_data.append(backup_server_hrisk)
+        risk_data.append(backup_server_mrisk)
+
+    except ValueError:
+        backup_server_hrisk = 0
+        backup_server_mrisk = 0
+        risk_data.append(backup_server_hrisk)
+        risk_data.append(backup_server_mrisk)
+
+    try:
+        row_dns_server = col_data.index('10.232.80.1')
+        dns_server_hrisk = int(sheet.cell_value(row_dns_server, 5))
+        dns_server_mrisk = int(sheet.cell_value(row_dns_server, 6))
+        risk_data.append(dns_server_hrisk)
+        risk_data.append(dns_server_mrisk)
+    except ValueError:
+        dns_server_hrisk = 0
+        dns_server_mrisk = 0
+        risk_data.append(dns_server_hrisk)
+        risk_data.append(dns_server_mrisk)
+    try:
+        row_desk_server = col_data.index('10.232.80.54')
+        desk_server_hrisk = int(sheet.cell_value(row_desk_server, 5))
+        desk_server_mrisk = int(sheet.cell_value(row_desk_server, 6))
+        risk_data.append(desk_server_hrisk)
+        risk_data.append(desk_server_mrisk)
+    except ValueError:
+        desk_server_hrisk = 0
+        desk_server_mrisk = 0
+        risk_data.append(desk_server_hrisk)
+        risk_data.append(desk_server_mrisk)
+    try:
+        row_deskbackup_server = col_data.index('10.232.80.53')
+        deskbackup_server_hrisk = int(
+            sheet.cell_value(row_deskbackup_server, 5))
+        deskbackup_server_mrisk = int(
+            sheet.cell_value(row_deskbackup_server, 6))
+        risk_data.append(deskbackup_server_mrisk)
+        risk_data.append(deskbackup_server_mrisk)
+    except ValueError:
+        deskbackup_server_hrisk = 0
+        deskbackup_server_mrisk = 0
+        risk_data.append(deskbackup_server_mrisk)
+        risk_data.append(deskbackup_server_mrisk)
+
+    return risk_data
 
 
 if __name__ == "__main__":
-    L = file_name('./')
-    a ="./"
-  
-    doc()
-    for num in range(2):
-        un_zip(L[num], str(num))
+    all_zip_file = file_name('./')
+    root = "./"
+    target_file = []
+    output_word = []
+    safe_report = []
+    document = Document()
+    document.save('report.docx')
+    for num in range(4):
+        un_zip(all_zip_file[num], str(num))
+        name_temp = (root + str(num) + '\index.xls')
+        target_file.append(name_temp)
+    risk_data = read_excel(target_file[2])
+    print risk_data
+    
 
-        xlsfile = a+str(num)+'\index.xls'
-        print xlsfile
-        read_excel(xlsfile)
-        write_excel(xlsfile)
+    safe_report.append(u'本月使用绿盟扫描，')
+    safe_report.append(u'门户服务器：')
+    safe_report.append(str(risk_data[0]) + u'个高危漏洞（已备案），')
+    safe_report.append(str(risk_data[1]) + u'个中危漏洞（已备案），')
+    safe_report.append(u'DNS服务器：')
+    safe_report.append(str(risk_data[2]) + u'个高危漏洞（已备案），')
+    safe_report.append(str(risk_data[3]) + u'个中危漏洞（已备案），')
+    safe_report.append(u'备份服务器：')
+    safe_report.append(str(risk_data[4]) + u'个高危漏洞（已备案），')
+    safe_report.append(str(risk_data[5]) + u'个中危漏洞（已备案），')
+    safe_report.append(u'桌管服务器：')
+    safe_report.append(str(risk_data[6]) + u'个高危漏洞（已备案），')
+    safe_report.append(str(risk_data[7]) + u'个中危漏洞（已备案），')
+    safe_report.append(u'桌管备份服务器：')
+    safe_report.append(str(risk_data[8]) + u'个高危漏洞（已备案），')
+    safe_report.append (str(risk_data[9]) + u'个中危漏洞（已备案），')
+    safe_report.append(u'外网主机、服务器、终端均未发现中、高危漏洞。自建系统未发现高、中危漏洞。具体扫描结果见附件。')
+    
+    output_word.append(safe_report[0])
+
+    for i in range(5):
+        if  risk_data[2*i]!=0 or risk_data[2*i+1] != 0:
+            output_word.append(safe_report[3*i+1])
+        if risk_data[2*i]!=0:
+            output_word.append(safe_report[3*i+2])
+        if risk_data[2*i+1]!=0:
+            output_word.append(safe_report[3*i+3])
+    output_word.append(safe_report[16])     
+    doc(output_word)
+   
+    # if risk_data[0] == 0 and risk_data[1] == 0:
+    #     pass
+    # else:
+    #     output_word.append(safe_report_2)
+    # if risk_data[2] == 0 and risk_data[3] == 0:
+    #     pass
+    # else:
+    #     output_word.append(safe_report_3)
+    # if risk_data[4] == 0 and risk_data[5] == 0:
+    #     pass
+    # else:
+    #     output_word.append(safe_report_4)
+    # if risk_data[6] == 0 and risk_data[7] == 0:
+    #     pass
+    # else:
+    #     output_word.append(safe_report_5)
+    # if risk_data[8] == 0 and risk_data[9] == 0:
+    #     pass
+    # else:
+    #     output_word.append(safe_report_6)
+
+
+
 
     print("ok")
  #   read_excel(index.xls)
